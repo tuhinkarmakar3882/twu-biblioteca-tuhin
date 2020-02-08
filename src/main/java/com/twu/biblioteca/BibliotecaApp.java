907013aa5ea4f0e74053c454f8c386da40c72f31
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-02-08 12:04:49 +0530

    <DONE> Requirement 1.1 | Add. The App now Shows a Welcome Message, Everytime When It Starts | Tuhin

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
 	public static void main(String[] args) {
-
+		WelcomeScreen welcomeScreen = new WelcomeScreen("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+		welcomeScreen.showMessage();
 	}
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,6 +1,27 @@
 package com.twu.biblioteca;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.*;
+
 class BibliotecaAppTest {
+	PrintStream mockPrintStream;
 
+	@BeforeEach
+	public void setup() {
+		mockPrintStream = mock(PrintStream.class);
+		System.setOut(mockPrintStream);
+	}
 
+	@Test
+	void testShouldPrintAWelcomeMessageWheneverTheApplicationStarts() {
+		String expectedWelcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+		BibliotecaApp.main(null);
+
+		verify(mockPrintStream, times(1)).println(expectedWelcomeMessage);
+	}
 }
